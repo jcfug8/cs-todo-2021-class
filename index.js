c9f2441f8627662b9f24a4a157aafb4a16fcfaa1
Jace Fugate <jace.fugate@tcnbroadcasting.com>
2021-06-23 16:27:22 -0600

    more stuff

--- index.js
+++ index.js
@@ -1,14 +1,33 @@
 // This file is in charge of starting the application
 const server = require("./server");
 const persist = require("./persist");
+const fs = require("fs");
 
 // define a port
 const portNumber = process.argv[2] || 8080;
 
-// connect to the database
-persist.connect(function () {
-  // start the server
-  server.listen(portNumber, () => {
-    console.log(`Running Server on Port ${portNumber}`);
+// // connect to the database
+// persist.connect(function () {
+//   // start the server
+//   server.listen(portNumber, () => {
+//     console.log(`Running Server on Port ${portNumber}`);
+//   });
+// });
+
+function readFile(filePath, callback) {
+  fs.readFile(filePath, "utf-8", (err, content) => {
+    console.log("content in readFile", content);
+    let counter = 0;
+    callback(counter, content);
+    counter++;
+    callback(counter, content);
+    counter++;
+    callback(counter, content);
+    counter++;
+    callback(counter, content);
   });
+}
+
+readFile("myfile.txt", (counter, content) => {
+  console.log(counter, " content in not readfile", content);
 });
