a1690a2c7e0b545cfa7890a46e6ad58d65726712
Jace Fugate <jace.fugate@tcnbroadcasting.com>
2021-06-23 20:45:25 -0600

    fix commented out stuff

--- index.js
+++ index.js
@@ -6,28 +6,28 @@ const fs = require("fs");
 // define a port
 const portNumber = process.argv[2] || process.env.PORT || 8080;
 
-// // connect to the database
-// persist.connect(function () {
-//   // start the server
-//   server.listen(portNumber, () => {
-//     console.log(`Running Server on Port ${portNumber}`);
-//   });
-// });
-
-function readFile(filePath, callback) {
-  fs.readFile(filePath, "utf-8", (err, content) => {
-    console.log("content in readFile", content);
-    let counter = 0;
-    callback(counter, content);
-    counter++;
-    callback(counter, content);
-    counter++;
-    callback(counter, content);
-    counter++;
-    callback(counter, content);
+// connect to the database
+persist.connect(function () {
+  // start the server
+  server.listen(portNumber, () => {
+    console.log(`Running Server on Port ${portNumber}`);
   });
-}
-
-readFile("myfile.txt", (counter, content) => {
-  console.log(counter, " content in not readfile", content);
 });
+
+// function readFile(filePath, callback) {
+//   fs.readFile(filePath, "utf-8", (err, content) => {
+//     console.log("content in readFile", content);
+//     let counter = 0;
+//     callback(counter, content);
+//     counter++;
+//     callback(counter, content);
+//     counter++;
+//     callback(counter, content);
+//     counter++;
+//     callback(counter, content);
+//   });
+// }
+
+// readFile("myfile.txt", (counter, content) => {
+//   console.log(counter, " content in not readfile", content);
+// });
